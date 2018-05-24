be36c29e160b24618497dc697f60d2ae45394bb5
sun1ive <sunliveua@gmail.com>
2018-05-24 15:11:35 +0300

    gettin better

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,18 @@
 // @flow
+import fastify from 'fastify';
+import morgan from 'morgan';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = fastify();
+
+app.get('/', async (request, reply) => {
+  try {
+    reply.send('hello world');
+  } catch (e) {
+    reply.send(e);
+  }
+});
+
+
+app.listen(8081, () => console.log('Server is running at port 8081'));
