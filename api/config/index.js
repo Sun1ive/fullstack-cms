d6b469d7c11db9a8f521395dd7bc1b9469f9955b
Sun1ive <sunliveua@gmail.com>
2018-05-26 11:03:24 +0300

    gettin better

---
+++ api/config/index.js
@@ -0,0 +1,7 @@
+export default {
+  db: {
+    MONGOUSER: 'root',
+    MONGOPW: 'example',
+    MONGODBNAME: 'test',
+  },
+};
--- api/src/index.js
+++ api/src/index.js
@@ -3,8 +3,16 @@ import fastify from 'fastify';
 import morgan from 'morgan';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import config from '../config';
 
 const app = fastify();
+
+app.use(
+  cors({
+    origin: ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:8081'],
+  }),
+);
+app.use(morgan('dev'));
 
 app.get('/', async (request, reply) => {
   try {
@@ -14,5 +22,15 @@ app.get('/', async (request, reply) => {
   }
 });
 
-
-app.listen(8081, () => console.log('Server is running at port 8081'));
+app.listen(8081, () => {
+  mongoose
+    .connect(config.URL, {
+      db: config.db.MONGODBNAME,
+      poolSize: 10,
+    })
+    .then(() => {
+      /* eslint-disable no-console */
+      console.log('Connected to DB!');
+      console.log('Server is running at port 8081');
+    });
+});
